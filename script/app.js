4725842a825c7d31e07697da91be3084f3d5b9aa
Steph-6 <Steph.robinson95@gmail.com>
2017-04-05 01:13:06 +0100

    added animation

--- script/app.js
+++ script/app.js
@@ -1,4 +1,7 @@
-$(function() {
+window.onload = init;
+
+function init() {
+// *********** links ***********
   $('#email').css('display', 'none');
   $('.email-link').on('mouseenter', function() {
     $('#email').fadeIn(5000);
@@ -22,88 +25,66 @@ $(function() {
     $('.in').removeClass('rotate');
   });
 
-  $('a').on('click', function() {
-    $('body').css('border', '6px solid rgba(50,50,50,0.3)');
-    $('body').css('filter', 'blur(2px)');
-    // setTimeout(function() {
-      // $('body').css('border', '2px solid rgba(50,50,50,0.3)');
-    // }, 500);
+// *********** game ************
+
+  const canvas = $('#snake');
+  const ctx    = snake.getContext('2d');
+  let raf      = null;
+
+  console.log(canvas);
+  console.log(ctx);
+
+  var ball = {
+    x: 50,
+    y: 50,
+    vx: 5,
+    vy: 2,
+    radius: 10,
+    color: 'black',
+    draw: function() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+  };
+
+  function draw() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ball.draw();
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    if (ball.y + ball.vy > canvas.height ||
+        ball.y + ball.vy < 0) {
+      ball.vy = -ball.vy;
+    }
+    if (ball.x + ball.vx > canvas.width ||
+        ball.x + ball.vx < 0) {
+      ball.vx = -ball.vx;
+    }
+
+    raf = window.requestAnimationFrame(draw);
+  }
+
+  canvas.on ('mouseover', function(e) {
+    raf = window.requestAnimationFrame(draw);
   });
 
-  setInterval(function() {
-    const choose = chooseSquare(1,13);
-    $(`#row${choose}`).css('border-bottom', '0px');
-  }, 300);
+  canvas.on('mouseout', function(e) {
+    window.cancelAnimationFrame(raf);
+  });
 
-  setInterval(function() {
-    const choose = chooseSquare(1,13);
-    $(`#row${choose}`).css('border-bottom', 'solid 2px white');
-  }, 300);
-
-
-  // $('#about').on('click', function() {
-  //   if (projects === true) {
-  //     $('.project-content').remove();
-  //   }
-  //   if (about === false) {
-  //     $('.content').append('<div class="about-content">hello you</div>');
-  //   }
-  //   about = true;
-  //   projects = false;
-  // });
-  //
-  // $('.projects').on('click', function() {
-  //   const id = $(this).attr('id');
-  //   if (about === true) {
-  //     $('.about-content').remove();
-  //   }
-  //   if (projects === false) {
-  //     // $('.content').append($project + id);
-  //     console.log(id);
-  //   }
-  //   projects = true;
-  //   about = false;
-  // });
-
-  // var canvas = $('#paper')[0];
-  // var c = canvas.getContext('2d');
-  //
-  // var startX = 50;
-  // var startY = 50;
-  // var endX = 100;
-  // var endY = 100;
-  // var amount = 0;
-  // setInterval(function() {
-  //     amount += 0.05; // change to alter duration
-  //     if (amount > 1) amount = 1;
-  //     c.clearRect(0, 0, canvas.width, canvas.height);
-  //     c.strokeStyle = 'black';
-  //     c.moveTo(startX, startY);
-  //     // lerp : a  + (b - a) * f
-  //     c.lineTo(startX + (endX - startX) * amount, startY + (endY -  startY) * amount);
-  //     c.stroke();
-  //   }, 30);
-
-  // buildGrid();
-  //
-  // function buildGrid() {
-  //   for(var x = 0; x < 100; x++) {
-  //     for(var y = 0; y < 100; y++) {
-  //       var unit = $(`<div class='unit' id=${x,y}></div>`);
-  //       unit.appendTo('#container');
-  //     }
-  //   }
-    // setInterval(function() {
-    //   let colored = chooseSquare(0,100);
-    //   $(`#${colored}`).css('background-color', 'red');
-    // }, 30);
+  ball.draw();
+}
 
 
 
-
-  function chooseSquare(a, b) {
-    min = Math.ceil(a);
-    max = Math.floor(b);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-});
+// $('a').on('click', function() {
+//   $('body').css('border', '6px solid rgba(50,50,50,0.3)');
+//   $('body').css('filter', 'blur(2px)');
+  // setTimeout(function() {
+    // $('body').css('border', '2px solid rgba(50,50,50,0.3)');
+  // }, 500);
+// });
