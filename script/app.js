994b3433c1838892e0ff1d646fd1b57f8963b0d5
Steph-6 <Steph.robinson95@gmail.com>
2017-04-07 23:27:22 +0100

    comments added

--- script/app.js
+++ script/app.js
@@ -1,7 +1,6 @@
 window.onload = init;
 
 function init() {
-// *********** links ***********
   $('#email').css('display', 'none');
   $('.email-link').on('mouseenter', function() {
     $('#email').fadeIn(5000);
@@ -24,26 +23,6 @@ function init() {
   $('.linkedin').on('mouseleave', function() {
     $('.in').removeClass('rotate');
   });
+
+  // only on change of window between title and portfolio animate the logo and name 
 }
-// *********** game ************
-
-
-
-// $('.start').on('click', function() {
-//   console.log('clicked');
-//   // raf = window.requestAnimationFrame(draw);
-//   $('.start').addClass('pause');
-//   $('.start').removeClass('start');
-// });
-//
-// $('.pause').on('click', function () {
-//   // window.cancelAnimationFrame(raf);
-// });
-
-// $('a').on('click', function() {
-//   $('body').css('border', '6px solid rgba(50,50,50,0.3)');
-//   $('body').css('filter', 'blur(2px)');
-  // setTimeout(function() {
-    // $('body').css('border', '2px solid rgba(50,50,50,0.3)');
-  // }, 500);
-// });
