052f770fb66820c942a2a5c6c3bbf8b6f862386a
Steph-6 <Steph.robinson95@gmail.com>
2017-06-24 00:32:53 +0100

    game footer added

--- script/app.js
+++ script/app.js
@@ -1,28 +1,57 @@
 window.onload = init;
 
 function init() {
-  $('#email').css('display', 'none');
-  $('.email-link').on('mouseenter', function() {
-    $('#email').fadeIn(5000);
-    $('#email-icon').addClass('rotate');
+  $('.jumping').hide();
+  $('.man').click(function(){
+    $('.man').hide();
+    setTimeout(function() {
+      $('.man').show();
+    }, 900);
+    $('.jumping').show();
+    setTimeout(function() {
+      $('.jumping').hide();
+    }, 900);
   });
-  $('.email-link').on('mouseleave', function() {
-    $('#email-icon').removeClass('rotate');
-  });
+  //  {
+  //   console.log('clicked');
+  //   // $('.man').css('display', 'none');
+  //   $('.man').hide('100', function() {
+  //
+  //   });
+  // });
 
-  $('.git').on('mouseenter', function() {
-    $('.mark').addClass('rotate');
-  });
-  $('.git').on('mouseleave', function() {
-    $('.mark').removeClass('rotate');
-  });
+  // function hide() {
+  //   $('.man').style.visibility = 'hidden';
+  //   setTimeout('show()', 2000);
+  // }
+  //
+  // function show() {
+  //   $('.man').style.visibility = 'visible';
+  //   setTimeout('hide()', 2000);
+  // }
 
-  $('.linkedin').on('mouseenter', function() {
-    $('.in').addClass('rotate');
-  });
-  $('.linkedin').on('mouseleave', function() {
-    $('.in').removeClass('rotate');
-  });
+  // $('#email').css('display', 'none');
+  // $('.email-link').on('mouseenter', function() {
+  //   $('#email').fadeIn(5000);
+  //   $('#email-icon').addClass('rotate');
+  // });
+  // $('.email-link').on('mouseleave', function() {
+  //   $('#email-icon').removeClass('rotate');
+  // });
+  //
+  // $('.git').on('mouseenter', function() {
+  //   $('.mark').addClass('rotate');
+  // });
+  // $('.git').on('mouseleave', function() {
+  //   $('.mark').removeClass('rotate');
+  // });
+  //
+  // $('.linkedin').on('mouseenter', function() {
+  //   $('.in').addClass('rotate');
+  // });
+  // $('.linkedin').on('mouseleave', function() {
+  //   $('.in').removeClass('rotate');
+  // });
 
-  // only on change of window between title and portfolio animate the logo and name 
+  // only on change of window between title and portfolio animate the logo and name
 }
